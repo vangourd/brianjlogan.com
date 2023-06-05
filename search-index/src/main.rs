f11756760bf0e03311948ea354e605c2643b2947
Brian Logan <mail@brianjlogan.com>
2023-06-05 13:24:50 -0400

    Working on search-indexer

--- search-index/src/main.rs
+++ search-index/src/main.rs
@@ -1,3 +1,13 @@
+use std::fs;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    // Grab posts from posts dir
+    let paths = fs::read_dir(&args[1]).unwrap();
+    for path in paths {
+        println!("Name: {}", path.unwrap().path().display())
+    }
+    // Convert into tokens
+    // Write to a flat file in a serverable folder
 }
