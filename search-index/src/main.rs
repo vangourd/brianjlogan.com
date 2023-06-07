84d057dc2ab105b12e3e6b2e2db809295a3fa7f6
Brian Logan <id@brianjlogan.com>
2023-06-07 00:20:25 -0400

    Added rudimentary tokenization

--- search-index/src/main.rs
+++ search-index/src/main.rs
@@ -1,12 +1,24 @@
 use std::fs;
+use std::fs::File;
 use std::env;
+use std::io::Split;
+use std::str;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     // Grab posts from posts dir
     let paths = fs::read_dir(&args[1]).unwrap();
+
     for path in paths {
-        println!("Name: {}", path.unwrap().path().display())
+
+        let path_str = path.unwrap().path().display().to_string();
+        let data = fs::read_to_string(&path_str).expect("Unable to read file");
+        let tokens = str::split_whitespace(&data);
+        println!("Tokens for :{}",path_str);
+        for token in tokens {
+            println!("{}", token)
+        }
+
     }
     // Convert into tokens
     // Write to a flat file in a serverable folder
