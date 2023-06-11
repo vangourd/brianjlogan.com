3cac68cfdfca0122b20137a977fec791fdf033d5
Brian Logan <mail@brianjlogan.com>
2023-06-10 20:43:00 -0400

    Continued refactoring

--- search-index/src/main.rs
+++ search-index/src/main.rs
@@ -9,25 +9,26 @@ use std::collections::{HashMap,HashSet};
 use serde::{Serialize, Deserialize};
 use serde_json::Result;
 
-fn tokens_from_file(path: Result<DirEntry, Error>, stop_words: HashSet<&str>) -> Result<SplitWhitespace, Error> {
-
-    let path_str = path?.path().display().to_string();
-    let file_data = fs::read_to_string(&path_str).expect("Unable to read file");
-    let tokens = str::split_whitespace(&file_data);
-    tokens.filter(|token| !stop_words.contains(token)).collect();
-    Ok((tokens))
+fn tokens_from_file(path: DirEntry, stop_words: HashSet<&str>) -> Result<Vec<&str>> {
+    let path_str = path.path().display().to_string();
+    let tokens = fs::read_to_string(&path_str)
+        .expect("Unable to read file")
+        .split_whitespace()
+        .filter(|t| stop_words.contains(t))
+        .collect();
+    return Ok(tokens)
 }
 
-fn write_index_to_file() {
-
-}
-
-fn index_as_json(s: SplitWhitespace) -> Result<String, Error>{
-
+fn write_to_file(path: &str, data: String,) {
+    let mut file = File::create(&path).unwrap();
+    file.write_all(data.as_bytes());
 }
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
+    let mut output_file = String::new();
+    output_file.push_str(&args[2]);
+    output_file.push_str("index.json");
 
     let names_from_dir = fs::read_dir(&args[1])
         .expect("Problem getting post names from directory");
@@ -35,19 +36,17 @@ fn main() -> std::io::Result<()> {
     let mut inverted_index = HashMap::new();
 
     for path in names_from_dir {
-        tokens = tokens_from_file(path, stop_words=HashSet::from(["a","the","an","#"]))
-        inverted_index.insert(token.to_string(), path.to_string());
+        let tokens = tokens_from_file(path?, HashSet::from(["a","the","an","#"]));
+        for token in tokens {
+            inverted_index.insert(token, path.unwrap().path().display().to_string());
+        }
     }
 
-    write_index_to_file(index_as_json)
-
-    let json = serde_json::to_string_pretty(&inverted_index);
-    let mut output_file = String::new();
-    output_file.push_str(&args[2]);
-    output_file.push_str("index.json");
-    let mut file = File::create(&output_file).unwrap();
-    file.write_all(json.unwrap().as_bytes());
-    Ok(())
+    
+    write_to_file(&output_file,serde_json::to_string_pretty(&inverted_index)
+        .expect("Problem converting to json"));
+    
+    return Ok(())
     // Convert into tokens
     // Write to a flat file in a serverable folder
 }
