5f6338badcdacf5068ace92626bad089d1ca67d6
Brian Logan <id@brianjlogan.com>
2023-06-11 21:31:23 -0400

    Refactor in progress, structure working, index not being built correctly need to debug

--- search-index/src/main.rs
+++ search-index/src/main.rs
@@ -1,17 +1,12 @@
-use std::fs::{self, DirEntry};
+use std::fs::{self};
 use std::fs::File;
 use std::env;
-use std::hash::Hash;
-use std::io::{Split,Write};
-use std::io::prelude::*;
-use std::str::{self, SplitWhitespace};
+use std::io::{Write};
 use std::collections::{HashMap,HashSet};
-use serde::{Serialize, Deserialize};
 use serde_json::Result;
 
-fn tokens_from_file(path: DirEntry, stop_words: HashSet<&str>) -> Result<Vec<String>> {
-    let path_str = path.path().display().to_string();
-    let tokens = fs::read_to_string(&path_str)
+fn tokens_from_file(path: &String, stop_words: HashSet<&str>) -> Result<Vec<String>> {
+    let tokens = fs::read_to_string(path)
         .expect("Unable to read file")
         .split_whitespace()
         .filter(|t| stop_words.contains(t))
@@ -20,27 +15,41 @@ fn tokens_from_file(path: DirEntry, stop_words: HashSet<&str>) -> Result<Vec<Str
     return Ok(tokens)
 }
 
-fn write_to_file(path: &str, data: String,) {
+fn write_to_file(path: &str, data: String) {
     let mut file = File::create(&path).unwrap();
-    file.write_all(data.as_bytes());
+    file.write_all(data.as_bytes())
+        .expect("Unable to write index file");
 }
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
+    let index_dir = &args.get(2)
+        .expect("Index dir not specified");
+    let posts_dir = &args.get(1)
+        .expect("Posts dir not specified");
     let mut output_file = String::new();
-    output_file.push_str(&args[2]);
+  
+    output_file.push_str(&index_dir);
     output_file.push_str("index.json");
 
-    let names_from_dir = fs::read_dir(&args[1])
+    let names_from_dir = fs::read_dir(&posts_dir)
         .expect("Problem getting post names from directory");
     
     let mut inverted_index = HashMap::new();
 
     for path in names_from_dir {
-        let tokens = tokens_from_file(path?, HashSet::from(["a","the","an","#"]));
-        for token in tokens {
-            inverted_index.insert(token, path.unwrap().path().display().to_string());
+
+        let current_path = path.unwrap().path().display().to_string();
+
+        let tokens = tokens_from_file(
+            &current_path,
+            HashSet::from(["a","the","an","#"])
+        
+        );
+        for token in tokens.expect("Problem getting tokens from file"){
+            inverted_index.insert(token, current_path.clone());
         }
+            
     }
 
     
