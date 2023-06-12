1a112f41ca7b0c1c4084e7062165ad297f161dbc
Brian Logan <id@brianjlogan.com>
2023-06-11 21:35:57 -0400

    Refactor of search indexer complete

--- search-index/src/main.rs
+++ search-index/src/main.rs
@@ -9,29 +9,25 @@ fn tokens_from_file(path: &String, stop_words: HashSet<&str>) -> Result<Vec<Stri
     let tokens = fs::read_to_string(path)
         .expect("Unable to read file")
         .split_whitespace()
-        .filter(|t| stop_words.contains(t))
+        .filter(|t| !stop_words.contains(t))
         .map(|s| s.to_string())
         .collect();
     return Ok(tokens)
 }
 
 fn write_to_file(path: &str, data: String) {
-    let mut file = File::create(&path).unwrap();
+    let mut file = File::create(&path)
+        .expect("Unable to create file. Does index directory exist or have correct permissions?");
     file.write_all(data.as_bytes())
         .expect("Unable to write index file");
 }
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    let index_dir = &args.get(2)
-        .expect("Index dir not specified");
     let posts_dir = &args.get(1)
         .expect("Posts dir not specified");
     let mut output_file = String::new();
-  
-    output_file.push_str(&index_dir);
-    output_file.push_str("index.json");
-
+    output_file.push_str("index/main.json");
     let names_from_dir = fs::read_dir(&posts_dir)
         .expect("Problem getting post names from directory");
     
@@ -43,7 +39,7 @@ fn main() -> std::io::Result<()> {
 
         let tokens = tokens_from_file(
             &current_path,
-            HashSet::from(["a","the","an","#"])
+            HashSet::from(["a","the","an","#","is"])
         
         );
         for token in tokens.expect("Problem getting tokens from file"){
