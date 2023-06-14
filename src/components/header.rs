547bc5b179c463d0d1c9a39045b89fd137fb9fd6
Brian Logan <id@brianjlogan.com>
2023-06-13 22:20:52 -0400

    Went a way more refined direction for styling

--- src/components/header.rs
+++ src/components/header.rs
@@ -4,15 +4,20 @@ use sycamore::prelude::*;
 pub fn Header<G: Html>(cx: Scope) -> View<G> {
     view! { cx,
         div(class="header") {
-            img(alt="picture of brian", src="images/profile.png",class="profile-photo")
-            h1{"BrianJlogan.com"}
-            div(class="sub_header"){
-                p{
-                "let's talk: " 
-                    strong{a(href="#"){"everything "}}
-                    a{"tech food biking"}
-                    input(placeholder="search"){""}
+            div(class="top_header"){
+                img(alt="picture of brian", src="images/profile.png",class="profile-photo")
+                h1{"brianjlogan.com"}
             }
+            nav{
+                ul{
+                    li{"programming"}
+                    li{"dev-sec-ops"}
+                    li{"food"}
+                    li{"biking"}
+                    li{"contact"}
+                    li{"resume"}
+                }
+                input(placeholder="🔎"){""}
             }
 
         }
