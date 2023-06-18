f34bd8af3a0c80d1a1176816dde999af4a9efe2f
Brian Logan <mail@brianjlogan.com>
2023-06-18 13:33:34 -0400

    Just easy printing the search index

--- src/components/post_list.rs
+++ src/components/post_list.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use sycamore::prelude::*;
 use mdsycx::{parse,MDSycX};
 use crate::model::{posts::{get_post}, index::get_inverted_index};
@@ -5,45 +7,16 @@ use crate::model::{posts::{get_post}, index::get_inverted_index};
 #[component]
 pub async fn PostList<G: Html>(cx: Scope<'_>) -> View<G> {
 
-        let index = get_inverted_index()
+        let index = get_inverted_index().await.unwrap();
 
-        let post = String::from("test.md");
+        let map: HashMap<String, String> = serde_json::from_str(&index).unwrap();
 
-        let req = get_post(post);
-
-        match req.await {
-            Ok(value) => {
-                match parse::<()>(create_ref(cx, value)) {
-                    Ok(parsed) => {
-                        view! { cx,
-                            div (class="post") {
-                                MDSycX(body=parsed.body)
-                            }
-                        }
-                    }
-                    Err(error) => {
-                        println!("{error:?}");
-                        view! { cx,
-                            table {
-                                tbody {
-                                    (format!("Unable to render post."))
-                                }
-                            }
-                        }
-                    }
-                } 
-            }
-            Err(error) => {
-                println!("{error:?}");
-                view! { cx,
-                    table {
-                        tbody {
-                            (format!("Unable to fetch post."))
-                        }
-                    }
+        view! { cx,
+            table {
+                tbody {
+                    (format!("{:?}", map))
                 }
             }
         }
-    
-    
+
 }
