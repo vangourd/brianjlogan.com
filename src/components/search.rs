eda3f8366d5f09a77549d7ad8d2637943eb3fc09
Brian Logan <mail@brianjlogan.com>
2023-07-16 09:05:30 -0400

    Figured out reactive search results

--- src/components/search.rs
+++ src/components/search.rs
@@ -1,4 +1,5 @@
 use std::{collections::HashMap, result};
+use serde::de::Visitor;
 use sycamore::prelude::*;
 use mdsycx::{parse,MDSycX};
 use crate::model::posts::{get_post, get_inverted_index};
@@ -6,21 +7,17 @@ use crate::model::posts::{get_post, get_inverted_index};
 #[component]
 pub async fn Search<G: Html>(cx: Scope<'_>) -> View<G> {
 
-        let search_terms = create_signal(cx, String::new());
-
-        let mut visible = 0;
-        create_effect(cx, move || 
-            if visible == 0 {
-                visible = 1;
+        let search_query = create_signal(cx, String::from(""));
+        let search_class = create_signal(cx, String::from("result hidden"));
+    
+        create_effect(cx, move || {
+            if search_query.get().len() > 0 {
+                search_class.set(String::from("result"))
             } else {
-                visible = 0;
+                search_class.set(String::from("result hidden"))
             }
-        );
-
-        let mut result: [String; 2] = [
-            String::from("result hidden"),
-            String::from("result")
-        ];
+                
+        });
         
 
         let index = get_inverted_index().await.unwrap();
@@ -28,11 +25,11 @@ pub async fn Search<G: Html>(cx: Scope<'_>) -> View<G> {
         view! { cx,
 
             div(class="searchbar"){
-                input(placeholder="🔎",bind:value=search_terms){""}
+                input(placeholder="🔎",bind:value=search_query){}
             }
-            ul(class=result[visible]){
+            ul(class=search_class.get()){
                 h2{"Found x results:"}
-                li(href="/post/"){
+                li(href="/post/"){ 
                     a(href="/post/"){"Post: My Resume"}
                 }
                 li(href="/tag/"){
