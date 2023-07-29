f6895b3bfae0f7b7c81e81c207465f5937d779a2
Brian Logan <mail@brianjlogan.com>
2023-07-28 20:43:07 -0400

    Working code path for types into map

--- search-index/src/main.rs
+++ search-index/src/main.rs
@@ -1,3 +1,4 @@
+use std::arch::x86_64::_bittest;
 use std::fs::{self};
 use std::fs::File;
 use std::env;
@@ -33,49 +34,25 @@ fn main() -> std::io::Result<()> {
     let mut output_file = String::new();
     output_file.push_str("index/main.json");
     // Collecting file names of posts
-    
+    let dir_ls = fs::read_dir(&posts_dir)
+        .expect("Problem getting post names from directory");
     // Building inverted index shell 
     let mut inverted_index: HashMap<String, HashMap<String, i32>> = HashMap::new();
 
     let stop_words: HashSet<&str> = HashSet::from(["a","the","an","#","is"]);
 
-    let names_from_dir: Vec<Option<&str>> = vec![];
+    for file in dir_ls{
 
-    // Iterating through posts
-    for path in names_from_dir.clone() {
-        if let Some(current_path) = path {
-            let current_path = current_path.to_string();
-            let tokens = tokens_from_file(
-                &current_path, 
-                stop_words.clone()).expect("Problem getting tokens from file");
+        let mut hm: HashMap<String, i32> = HashMap::new();
 
-            // Calculate term frequency (TF) for each term in the document 
-            let tf_map: HashMap<String, i32> = tokens.into_iter().fold(HashMap::new(), |mut acc, token | {
-                *acc.entry(token).or_insert(0) +=1 ;
-                acc
-            });
+        let t = String::from("token");
 
-            // Calculate the document frequency (DF) for each term in the document
-            let df_map: HashMap<String, i32> = tf_map.keys().fold(HashMap::new(), |mut acc, token| {
-                *acc.entry(token.to_lowercase()).or_insert(0) += 1;
-                acc
-            });
+        let score = 0;
 
-            // Calculate the total number of documents in the collection
-            let total_documents = names_from_dir.len() as i32;
+        hm.insert(file?.path().into_os_string().into_string().unwrap(), score);
+        
+        inverted_index.insert(t, hm);
 
-            // Calculate the IDF for each term and update the inverted index
-            for (token, tf) in tf_map {
-                let idf = 1.0 + (total_documents as f64 / (1.0 + df_map[&token.to_lowercase()] as f64)).ln();
-                let tf_idf_score = (tf as f64) * idf;
-
-                inverted_index
-                    .entry(token.to_lowercase())
-                    .or_insert_with(HashMap::new)
-                    .insert(current_path.clone(), tf_idf_score as i32);
-            }
-        }
- 
     }
     
     write_to_file(&output_file,serde_json::to_string_pretty(&inverted_index)
